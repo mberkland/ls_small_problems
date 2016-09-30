dd0182deaac3d21f917939d58bce0622f0cecb16
mberkland <miruke0506@hotmail.com>
2016-09-30 21:55:27 +0000

    small problems exercises

--- easy_5.rb
+++ easy_5.rb
@@ -182,3 +182,122 @@ def cleanup2(text)
 end
 
 # Question 6
+# Write a method that takes a string with one or more space separated words and
+# returns a hash that shows the number of words of different sizes.
+
+# Words consist of any string of characters that do not include a space.
+
+def word_sizes(phrase)
+  word_sizes_hash = {}
+  sizes = []
+  keys = []
+  phrase_array = phrase.split(" ")
+  phrase_array.each do |word|
+    sizes.push(word.count(word))
+  end
+  sizes.sort!
+  keys = sizes.uniq
+  keys.each do |key|
+    word_sizes_hash[key] = sizes.count(key)
+  end
+  p word_sizes_hash
+end
+
+word_sizes('Four score and seven.') # == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+word_sizes('Hey diddle diddle, the cat and the fiddle!') # == { 3 => 5, 6 => 1, 7 => 2 }
+word_sizes("What's up doc?") # == { 6 => 1, 2 => 1, 4 => 1 }
+word_sizes('') # == {}
+
+# Question 7
+# Modify the word_sizes method from the previous exercise to exclude non-letters
+# when determining word size. For instance, the length of "it's" is 3, not 4.
+
+def word_sizes(phrase)
+  word_sizes_hash = {}
+  sizes = []
+  keys = []
+  phrase_array = phrase.split(" ")
+  phrase_array.each do |word|
+    new_word = word.gsub(/[^a-z]/i, ' ').delete(' ')
+    sizes.push(word.count(new_word))
+  end
+  sizes.sort!
+  keys = sizes.uniq
+  keys.each do |key|
+    word_sizes_hash[key] = sizes.count(key)
+  end
+  p word_sizes_hash
+end
+
+word_sizes('Four score and seven.') # == { 3 => 1, 4 => 1, 5 => 2 }
+word_sizes('Hey diddle diddle, the cat and the fiddle!') # == { 3 => 5, 6 => 3 }
+word_sizes("What's up doc?") # == { 5 => 1, 2 => 1, 3 => 1 }
+word_sizes('') # == {}
+
+# Question 8
+# Write a method that takes an Array of Integers between 0 and 19, and returns
+# an Array of those Integers sorted based on the English words for each number:
+
+# zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven,
+# twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen
+
+NUMBER_TO_LETTER = { 0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four',
+                     5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine',
+                     10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thriteen',
+                     14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
+                     17 => 'seventeen', 18 => 'eightteen', 19 => 'nineteen' }
+
+def alphabetic_number_sort(num_arr)
+  letter_array = []
+  sorted_num_array = []
+  num_arr.each do |number|
+    letter_array.push(NUMBER_TO_LETTER[number])
+  end
+  letter_array.sort.each do |word|
+    sorted_num_array.push(NUMBER_TO_LETTER.key(word))
+  end
+  sorted_num_array
+end
+
+
+p alphabetic_number_sort((0..19).to_a) # ==
+[
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
+
+# Question 9
+# Write a method that takes a string argument and returns a new string that
+# contains the value of the original string with all consecutive duplicate
+# characters collapsed into a single character.
+
+def crunch(phrase)
+  phrase.squeeze
+end
+
+p crunch('ddaaiillyy ddoouubbllee') # == 'daily double'
+p crunch('4444abcabccba') # == '4abcabcba'
+p crunch('ggggggggggggggg') # == 'g'
+p crunch('a') # == 'a'
+p crunch('')  # == ''
+
+# Question 10
+# Write a method that will take a short line of text, and print it within a box.
+
+def print_in_box(phrase)
+  length = phrase.length
+  half_length = length/2
+  if length % 2 == 0
+    space_end = half_length - 1
+  else
+    space_end = half_length
+  end
+  puts "+-#{"-"*(length * 2)}-+"
+  puts "| #{" " * length}#{" " * length} |"
+  puts "| #{" " * half_length}#{phrase}#{" " * space_end}  |"
+  puts "| #{" " * length}#{" " * length} |"
+  puts "+-#{"-"*(length * 2)}-+"
+  puts phrase
+end
+
+print_in_box('To boldly go where no one has gone before.')
