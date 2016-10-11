ad17deafaf454379f401b5eef6f5a798f5f11ef0
mberkland <miruke0506@hotmail.com>
2016-10-11 19:29:38 +0000

    small problems exercises

--- medium_1.rb
+++ medium_1.rb
@@ -160,3 +160,121 @@ def diamond(num)
 end
 
 diamond(9)
+
+# Question 6
+# Write a method that implements a miniature stack-and-register-based programming
+# language that has the following commands:
+
+# n Place a value n in the "register". Do not modify the stack.
+# PUSH Push the register value on to the stack. Leave the value in the register.
+# ADD Pops a value from the stack and adds it to the register value, storing the
+#result in the register.
+# SUB Pops a value from the stack and subtracts it from the register value,
+# storing the result in the register.
+# MULT Pops a value from the stack and multiplies it by the register value,
+# storing the result in the register.
+# DIV Pops a value from the stack and divides it into the register value,
+# storing the integer result in the register.
+# MOD Pops a value from the stack and divides it into the register value,
+# storing the integer remainder of the division in the register.
+# POP Remove the topmost item from the stack and place in register
+# PRINT Print the register value
+# All operations are integer operations (which is only important with DIV and MOD).
+
+# Programs will be supplied to your language method via a string passed in as an
+# argument. Your program may assume that all programs are correct programs; that
+# is, they won't do anything like try to pop a non-existent value from the stack,
+# and they won't contain unknown tokens.
+
+# You should initialize the register to 0.
+
+def string_number?(string)
+  string_arr = string.split(//)
+  string_num_array = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  string_arr.each do |item|
+    if string_num_array.include?(item)
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def minilang(string)
+  stack = []
+  register = 0
+  string_arr = string.split(" ")
+  string_arr.each do |item|
+    if string_number?(item)
+      register = item.to_i
+    elsif item == "PUSH"
+      stack.push(register)
+    elsif item == "ADD"
+      register += stack.pop
+    elsif item == "SUB"
+      register -= stack.pop
+    elsif item == "MULT"
+      register *= stack.pop
+    elsif item == "DIV"
+      register = register / stack.pop
+    elsif item == "MOD"
+      register = register % stack.pop
+    elsif item == "POP"
+      register = stack.pop
+    elsif item == "PRINT"
+      p register
+    end
+  end
+end
+
+minilang('5 PUSH 3 MULT PRINT')
+# # 15
+minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# # 5
+# # 3
+# # 8
+minilang('5 PUSH POP PRINT')
+# # 5
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# # 5
+# # 10
+# # 4
+# # 7
+minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# # 6
+minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# # # 12
+minilang('-3 PUSH 5 SUB PRINT')
+# 8 ------wrong here
+minilang('6 PUSH')
+# (nothing printed; no PRINT commands)
+
+# Question 7
+# Write a method that takes a sentence string as input, and returns a new string
+# that contains the original string with any sequence of the words 'zero', 'one',
+# 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' converted to a
+# string of digits.
+
+def word_to_digit(phrase)
+  numbers = {'zero' => 0, 'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4,
+            'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9}
+  phrase_arr = phrase.split(" ")
+  phrase_arr.map! do |word|
+    if word[-1] == "."
+      front = word.chop
+      if numbers.keys.include?(front)
+        word = numbers[front].to_s + "."
+      else
+        word
+      end
+    elsif numbers.keys.include?(word)
+      word = numbers[word]
+    else
+      word
+    end
+  end
+  p phrase_arr.join(" ")
+end
+
+word_to_digit('Please call me at five five five one two three four. Thanks.')
+# == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
