b510cc8c6520257f0c6e33a6ae776cb5da9b4358
mberkland <miruke0506@hotmail.com>
2016-11-14 17:18:38 +0000

    small problems practice exercises

--- advanced_1.rb
+++ advanced_1.rb
@@ -119,3 +119,41 @@ p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
 p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
 p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) == [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
 p transpose([[1]]) == [[1]]
+
+# Question 5
+# Write a method that takes an arbitrary matrix and rotates it 90 degrees clockwise
+
+def rotate90(matrix)
+	transposed_array = []
+	temp_array = []
+	counter = 0
+	loop do
+		matrix.reverse_each do |array|
+			temp_array.push(array[counter])
+		end
+		transposed_array.push(temp_array)
+		temp_array = []
+		counter += 1
+		break if counter == matrix[0].length
+	end
+	p transposed_array
+end
+
+matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8]
+]
+
+new_matrix1 = rotate90(matrix1)
+new_matrix2 = rotate90(matrix2)
+new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+
+p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
+p new_matrix3 == matrix2
